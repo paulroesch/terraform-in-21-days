116c2a9cf7924830c6703dd9dedc375094495d83
Paul Roesch <122225730+paulroesch@users.noreply.github.com>
2023-06-07 22:20:56 -0400

    added files .gitignore and main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>4.16"
+        }
+        
+    }
+
+    required_version = ">=1.2.0"
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "name" {
+  ami = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
+}
