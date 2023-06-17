12b8efbb11d0feed3e0916a61a13ee8e5cb36042
Paul Roesch <122225730+paulroesch@users.noreply.github.com>
2023-06-17 16:12:04 -0400

    priv ec2 instance removed, auto-scaling-group added

--- level2/instance.tf
+++ level2/instance.tf
@@ -85,26 +85,6 @@ resource "aws_security_group" "private" {
   }
 }
 
-resource "aws_instance" "private" {
-  count = length(data.terraform_remote_state.level1.outputs.private_subnet_id)
-
-  ami                    = data.aws_ami.amazonlinux.id
-  instance_type          = "t3.micro"
-  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[count.index]
-  vpc_security_group_ids = [aws_security_group.private.id]
-  key_name               = "main"
-  user_data              = file("user-data.sh")
-
-  tags = {
-    Name = "${var.env_code}-private"
-  }
-}
-
-
 output "public_ec2_public_ip" {
   value = aws_instance.public[*].public_ip
-}
-
-output "private_ec2_private_ip" {
-  value = aws_instance.private[*].private_ip
 }
